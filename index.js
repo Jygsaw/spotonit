87b1a60fcbdeaa382655b4638aa745b8057a7537
Justin Cheung <jygsaw@gmail.com>
2014-09-04 15:01:39 -0700

    Implement parsing of incoming targetUrl form; Implement fetch of targetUrl

--- index.js
+++ index.js
@@ -1,13 +1,52 @@
-var express = require('express')
+var express = require('express');
+var bodyParser = require('body-parser');
+var http = require('http');
 var app = express();
 
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({
+  extended: false,
+}));
 
-app.get('/', function(request, response) {
-  response.send('Hello World!')
-})
+app.get('/', function(req, res) {
+  res.sendfile('www/index.html');
+});
+
+app.post('/', function(req, res) {
+  // parse given target url
+  var targetUrl = req.body.targetUrl;
+  console.log("targetUrl:", targetUrl);
+
+  // fetch target url
+  http.get(targetUrl, function(res) {
+    console.log("Fetching:", targetUrl);
+    console.log("GET Status:", res.statusCode);
+
+    // collectData(res, function(data) {
+    //   console.log(data);
+    // });
+
+  }).on('error', function(e) {
+    console.log("Error retrieving:", targetUrl);
+    console.log("Error:", e.message);
+  });
+
+  var events = [];
+  res.send(200, events);
+});
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
-})
+});
+
+// collect data chunks
+function collectData(res, callback) {
+  var data = ''
+  res.on('data', function(chunk) {
+    data += chunk;
+  });
+  res.on('end', function() {
+    callback(data);
+  });
+};
